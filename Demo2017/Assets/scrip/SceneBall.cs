6b2e7e86dd6d74946e1dec31f9de9d64a1177978
Miidnight <chompununt.satj@bumail.net>
2017-01-28 17:09:56 +0700

    1551100090

---
+++ Demo2017/Assets/scrip/SceneBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneBall : MonoBehaviour {
+
+	public Text txtScore;
+	public Text txtAttack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	int score = 0;
+	int Attack = 0;
+	public void DopushButton(){
+		score++;
+		Attack+= 2;
+		txtScore.text = "Score :" + score.ToString ();
+		txtAttack.text = "Attack :" + Attack.ToString ();
+	}
+	public void DoChangeScene (){
+		SceneManager.LoadScene (1);
+}
+}
